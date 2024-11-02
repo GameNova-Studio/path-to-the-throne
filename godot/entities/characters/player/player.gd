41f03cb28a14f852b2eebfcbb2cfdefad7379b89
akpach <alvaro.akpc@gmail.com>
2024-11-02 00:28:33 -0500

    Se ha incluido codigo para que el personaje corra al presionar la tecla Shift

--- godot/entities/characters/player/player.gd
+++ godot/entities/characters/player/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 ## Velocidad de movimiento del personaje.
 @export_range(10, 1000, 10) var SPEED = 300.0
 
+## Velocidad de movimiento al correr del personaje.
+@export_range(10, 1000, 10) var RUN_SPEED = 500.0
+
 ## Referencia al nodo que contiene las animaciones del personaje.
 @onready var animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
 
@@ -11,26 +14,33 @@ func _physics_process(_delta: float) -> void:
 	velocity = Vector2.ZERO
 	if is_opening_door():
 		return
-	
+		
 	## Solo podemos movernos si no hay un dialogo abierto
 	if not Dialogue.is_dialogue_open():
+		##Variable temporal
+		var V_SPEED=SPEED
+		
+		#Verifica si se presiona se está presionando la tecla para correr.
+		if Input.is_action_pressed("sprint"):
+			V_SPEED=RUN_SPEED
+			
 		# Verifica si se está presionando la tecla para moverse a la izquierda. (X)
 		if Input.is_action_pressed("move_left"):
-			velocity.x= -SPEED
+			velocity.x= -V_SPEED
 			animated_sprite_2d.flip_h=true
 			
 		# Verifica si se está presionando la tecla para moverse hacia arriba. (Y)
 		if Input.is_action_pressed("move_up"):
-			velocity.y= -SPEED
+			velocity.y= -V_SPEED
 			
 		# Verifica si se está presionando la tecla para moverse a la derecha. (X)
 		if Input.is_action_pressed("move_right"):
-			velocity.x= SPEED
+			velocity.x= V_SPEED
 			animated_sprite_2d.flip_h=false
 			
 		# Verifica si se está presionando la tecla para moverse hacia abajo. (Y)
 		if Input.is_action_pressed("move_down"):
-			velocity.y= SPEED
+			velocity.y= V_SPEED
 	
 	# Cambia la animación según si el jugador esta intentando mover al
 	# personaje o no.
